fe3dbbafe6607c708b61f4feaf10fd8dc391c076
Shahrooz Bazrafshan <kingofday.b@gmail.com>
2023-11-20 15:08:18 +0330

    .

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,8 +1,9 @@
 import MultiSelect from "./MultiSelect"
 import RadioGroup from "./RadioGroup"
+import Radio from "./RadioGroup/Radio"
 import Checkbox from "./Checkbox"
 import Select from "./Select"
 import Input from "./Input"
 import SliderRange from "./SliderRange";
 import Option from "../Shared/Option"
-export { MultiSelect, RadioGroup, Select, Input, SliderRange, Checkbox, Option };
+export { MultiSelect, RadioGroup, Radio, Select, Input, SliderRange, Checkbox, Option };
