fc65fe8cff05c0a34ea83c90fb80f8bf65147353
masoud <masoudteymuri74@gmail.com>
2023-12-26 09:46:03 +0330

    Apply in the middle of keeping the tab active for tab buttons

---
+++ .storybook/stories/TabButtons.stories.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { TabButtons } from "../../src/components/Tab";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof TabButtons> = {
+  title: "/TabButtons",
+  component: TabButtons,
+};
+export default meta;
+type Story = StoryObj<typeof TabButtons>;
+export const DefaultTab: Story = {
+  render: () => {
+    return (
+      <TabButtons
+        // onChange={(tab) => handleChangeTab(tab as PageObjects, "category")}
+        // activeKey={tabState.objectTab ?? "symbol"}
+        variant="ink-bar"
+        key="category_tab_buttons"
+        tabs={[
+          {
+            key: "symbol",
+            text: "stocks",
+          },
+          {
+            key: "fund",
+            text: "fund",
+          },
+          {
+            key: "fund2",
+            text: "fund",
+          },
+          {
+            key: "fund3",
+            text: "fund2",
+          },
+          {
+            key: "fund4",
+            text: "fund4",
+          },
+          {
+            key: "fund5",
+            text: "fund5",
+          },
+          {
+            key: "fund6",
+            text: "fund6",
+          },
+          {
+            key: "fund7",
+            text: "fund7",
+          },
+        ]}
+      />
+    );
+  },
+};
--- src/components/Tab/TabButtons.tsx
+++ src/components/Tab/TabButtons.tsx
@@ -1,42 +1,53 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useRef, useState } from "react";
 import Button from "../Button";
 export interface TabItem {
-    key: string;
-    icon?: ReactNode;
-    className?: string;
-    text?: ReactNode;
-    disabled?: boolean;
-    [key: string]: any
+  key: string;
+  icon?: ReactNode;
+  className?: string;
+  text?: ReactNode;
+  disabled?: boolean;
+  [key: string]: any;
 }
 interface TabButtonsProps {
-    id?: string;
-    variant?: "pill" | "bordered" | "ink-bar";
-    initialActiveKey?: string;
-    activeKey?: string;
-    className?: string;
-    tabs: TabItem[];
-    hideScrollBar?: boolean;
-    onChange?: (key: string, tab?: TabItem) => void,
-    afterChange?: (key: string) => void,
+  id?: string;
+  variant?: "pill" | "bordered" | "ink-bar";
+  initialActiveKey?: string;
+  activeKey?: string;
+  className?: string;
+  tabs: TabItem[];
+  hideScrollBar?: boolean;
+  onChange?: (key: string, tab?: TabItem) => void;
+  afterChange?: (key: string) => void;
 }
-const TabButtons = ({
-    id,
-    initialActiveKey,
-    activeKey,
-    className,
-    onChange,
-    afterChange,
-    variant = "pill",
-    hideScrollBar,
-    tabs,
-}: TabButtonsProps) => {
-    const [innerActiveKey, chnageActiveKey] = useState(initialActiveKey ?? tabs.length ? tabs[0].key : "");
-    const handleClick = (key: string) => {
-        chnageActiveKey(key);
-        afterChange?.(key)
-    };
-    return <div id={id} className={`tab-buttons horizontal-scroll-bar ${variant}${className ? " " + className : ""}${hideScrollBar ? " hide-scroll-bar" : ""}`}>
-        {tabs.map((t) => <Button {...t} key={t.key} icon={t.icon} className={`${t.className ?? ""}${(onChange ? activeKey : innerActiveKey) === t.key ? " active" : ""}${t.disabled ? " disabled" : ""}`} variant="tab" onClick={() => t.disabled ? undefined : (onChange ? onChange(t.key, t) : handleClick(t.key))}>{t.text}</Button>)}
-    </div>;
+const TabButtons = ({ id, initialActiveKey, activeKey, className, onChange, afterChange, variant = "pill", hideScrollBar, tabs }: TabButtonsProps) => {
+  const [innerActiveKey, chnageActiveKey] = useState(initialActiveKey ?? tabs.length ? tabs[0].key : "");
+  const wrapperList = useRef<HTMLDivElement | null>(null);
+
+  const handleClick = (key: string) => {
+    chnageActiveKey(key);
+    afterChange?.(key);
+    const parentTabElement = wrapperList?.current as HTMLDivElement | undefined;
+    const activeElement = parentTabElement?.querySelector(`[data-key="${key}"]`) as HTMLUListElement | undefined;
+    if (parentTabElement && activeElement) {
+      parentTabElement.scrollLeft = activeElement.offsetLeft - (parentTabElement.offsetWidth - activeElement.offsetWidth) / 2;
+    }
+  };
+  return (
+    <div ref={wrapperList} id={id} className={`tab-buttons horizontal-scroll-bar ${variant}${className ? " " + className : ""}${hideScrollBar ? " hide-scroll-bar" : ""}`}>
+      {tabs.map((t) => (
+        <Button
+          {...t}
+          key={t.key}
+          data-key={t.key}
+          icon={t.icon}
+          className={`${t.className ?? ""}${(onChange ? activeKey : innerActiveKey) === t.key ? " active" : ""}${t.disabled ? " disabled" : ""}`}
+          variant="tab"
+          onClick={() => (t.disabled ? undefined : onChange ? onChange(t.key, t) : handleClick(t.key))}
+        >
+          {t.text}
+        </Button>
+      ))}
+    </div>
+  );
 };
 export default TabButtons;
