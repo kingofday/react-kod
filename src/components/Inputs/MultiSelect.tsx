6f247da88a084553d2123657c233f36be1fc6e78
masoud <masoudteymuri74@gmail.com>
2024-03-17 12:20:01 +0330

    added empty data for select and multi select

--- src/components/Inputs/MultiSelect.tsx
+++ src/components/Inputs/MultiSelect.tsx
@@ -6,149 +6,171 @@ import Badge from "../Badge";
 import Tag from "../Tag";
 import useOnClickOutside from "../../helpers/useOnClickOutside";
 import { createPortal } from "react-dom";
+import Empty from "../Shared/Empty";
 type Pos = "auto" | number;
 interface SelectProps {
-    name?: string;
-    id?: string;
-    values?: string[];
-    disabled?: boolean;
-    hasError?: boolean;
-    errorMessage?: string;
-    label?: string;
-    defaultValues?: string[];
-    className?: string;
-    placeholder?: string;
-    searchable?: boolean;
-    allowClear?: boolean;
-    searchText?: string;
-    suffix?: ReactNode;
-    popupTargetId?: string;
-    children?: ReactElement<SelectOptionItemProps> | ReactElement<SelectOptionItemProps>[];
-    onChange?: (values: string[]) => void;
-    showOptionOrder?: boolean;
+  name?: string;
+  id?: string;
+  values?: string[];
+  disabled?: boolean;
+  hasError?: boolean;
+  errorMessage?: string;
+  label?: string;
+  defaultValues?: string[];
+  className?: string;
+  placeholder?: string;
+  emptyLabel?: string;
+  emptyComponent?: ReactNode;
+  searchable?: boolean;
+  allowClear?: boolean;
+  searchText?: string;
+  suffix?: ReactNode;
+  popupTargetId?: string;
+  children?: ReactElement<SelectOptionItemProps> | ReactElement<SelectOptionItemProps>[];
+  onChange?: (values: string[]) => void;
+  showOptionOrder?: boolean;
 }
 const MultiSelect = ({
-    id,
-    name,
-    label,
-    values,
-    defaultValues,
-    className,
-    children,
-    placeholder,
-    searchable = false,
-    searchText,
-    suffix,
-    onChange,
-    showOptionOrder = true,
-    popupTargetId,
-    disabled = false
+  id,
+  name,
+  label,
+  values,
+  defaultValues,
+  className,
+  children,
+  placeholder,
+  emptyLabel,
+  emptyComponent,
+  searchable = false,
+  searchText,
+  suffix,
+  onChange,
+  showOptionOrder = true,
+  popupTargetId,
+  disabled = false,
 }: SelectProps) => {
-    const [isOpen, toggle] = useState(false);
-    const wrapperRef = useRef<HTMLDivElement>(null);
-    const ref = useRef<HTMLDivElement>(null);
-    const popupRef = useRef<HTMLUListElement | null>(null);
-    const popupTarget = useRef<HTMLElement | null>(null);
-    const searchRef = useRef<HTMLInputElement>(null);
-    const options = useRef<SelectOptionItemProps[]>(children ? (Array.isArray(children) ? children?.map(x => x.props) : [children.props]) : []);
-    const [popupStyle, setPopupStyle] = useState<CSSProperties | undefined>(undefined);
-    const [searchedOptions, setSearchedOptions] = useState<SelectOptionItemProps[]>([]);
-    const adjustPosition = () => {
-        const inputRect = ref.current?.getBoundingClientRect();
-        const popupRect = popupRef.current?.getBoundingClientRect();
-        const parentRect = popupTarget.current?.getBoundingClientRect();
-        if (!popupRect || !inputRect) return;
-        const h = window.innerHeight;
-        let left: Pos = "auto";
-        let top: Pos = "auto";
-        const scrollTop = window.scrollY || document.documentElement.scrollTop;
-        const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-        const inputOffsetTop = (wrapperRef.current?.offsetTop ?? 0) + (ref.current?.offsetTop ?? 0);
-        const inputOffsetLeft = (wrapperRef.current?.offsetLeft ?? 0) + (ref.current?.offsetLeft ?? 0);
-        const offsetHeight = popupTargetId && popupTarget.current ? popupTarget.current.offsetHeight - popupTarget.current.clientHeight : 0
-        const offsetWidth = popupTargetId && popupTarget.current ? popupTarget.current.offsetWidth - popupTarget.current.clientWidth : 0
+  const [isOpen, toggle] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const popupRef = useRef<HTMLUListElement | null>(null);
+  const popupTarget = useRef<HTMLElement | null>(null);
+  const searchRef = useRef<HTMLInputElement>(null);
+  const options = useRef<SelectOptionItemProps[]>(children ? (Array.isArray(children) ? children?.map((x) => x.props) : [children.props]) : []);
+  const [popupStyle, setPopupStyle] = useState<CSSProperties | undefined>(undefined);
+  const [searchedOptions, setSearchedOptions] = useState<SelectOptionItemProps[]>([]);
+  const adjustPosition = () => {
+    const inputRect = ref.current?.getBoundingClientRect();
+    const popupRect = popupRef.current?.getBoundingClientRect();
+    const parentRect = popupTarget.current?.getBoundingClientRect();
+    if (!popupRect || !inputRect) return;
+    const h = window.innerHeight;
+    let left: Pos = "auto";
+    let top: Pos = "auto";
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+    const inputOffsetTop = (wrapperRef.current?.offsetTop ?? 0) + (ref.current?.offsetTop ?? 0);
+    const inputOffsetLeft = (wrapperRef.current?.offsetLeft ?? 0) + (ref.current?.offsetLeft ?? 0);
+    const offsetHeight = popupTargetId && popupTarget.current ? popupTarget.current.offsetHeight - popupTarget.current.clientHeight : 0;
+    const offsetWidth = popupTargetId && popupTarget.current ? popupTarget.current.offsetWidth - popupTarget.current.clientWidth : 0;
 
-        if (inputRect.top + popupRect.height > h) {
-            top = ((popupTargetId && parentRect) ? inputOffsetTop : (inputRect.top + scrollTop)) - popupRect.height - offsetHeight;
-        }
-        else {
-            top = (popupTargetId && parentRect ? inputOffsetTop : (inputRect.top + scrollTop)) + inputRect.height - offsetHeight;
-        }
-        left = (popupTargetId && parentRect ? inputOffsetLeft : (inputRect.left + scrollLeft)) - offsetWidth;
-        setPopupStyle(({ top, left, right: "auto", width: inputRect.width }));
+    if (inputRect.top + popupRect.height > h) {
+      top = (popupTargetId && parentRect ? inputOffsetTop : inputRect.top + scrollTop) - popupRect.height - offsetHeight;
+    } else {
+      top = (popupTargetId && parentRect ? inputOffsetTop : inputRect.top + scrollTop) + inputRect.height - offsetHeight;
     }
-    const handleClose = () => {
-        toggle(false);
+    left = (popupTargetId && parentRect ? inputOffsetLeft : inputRect.left + scrollLeft) - offsetWidth;
+    setPopupStyle({ top, left, right: "auto", width: inputRect.width });
+  };
+  const handleClose = () => {
+    toggle(false);
+  };
+  const onSelect = (v: string) => {
+    if (values?.includes(v)) {
+      onChange?.(values?.filter((x) => x !== v));
+    } else {
+      onChange?.([...(values ?? []), v]);
     }
-    const onSelect = (v: string) => {
-        if (values?.includes(v)) {
-            onChange?.(values?.filter(x => x !== v));
-        }
-        else {
-            onChange?.([...values ?? [], v]);
-        }
-        if (searchable && searchRef.current?.value) {
-            searchRef.current.value = "";
-            setSearchedOptions(options.current);
-        }
-        toggle(false);
+    if (searchable && searchRef.current?.value) {
+      searchRef.current.value = "";
+      setSearchedOptions(options.current);
     }
-    const onSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        const { value } = e.target;
-        if (value.length)
-            setSearchedOptions(options.current.filter(x => x.value !== "" && ~(x.text ?? x.children as string).indexOf(value)));
-        else setSearchedOptions(options.current);
+    toggle(false);
+  };
+  const onSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (value.length) setSearchedOptions(options.current.filter((x) => x.value !== "" && ~(x.text ?? (x.children as string)).indexOf(value)));
+    else setSearchedOptions(options.current);
+  };
+  const removeOption = (v: string) => {
+    onChange?.(values?.filter((x) => x !== v) ?? []);
+  };
+  const selectedOptions = ((typeof values !== "undefined" ? values : defaultValues) ?? []).map((v) => options.current.find((x) => x.value === v)).filter((x) => !!x) as SelectOptionItemProps[]; // searchedOptions.filter(x => typeof values !== 'undefined' ? values.includes(x.value.toString()) : defaultValues?.includes(x.value));
+  useOnClickOutside([ref, popupRef], handleClose);
+  useEffect(() => {
+    popupTarget.current = popupTargetId ? document.getElementById(popupTargetId) : document.body;
+  }, [popupTargetId]);
+  useEffect(() => {
+    setSearchedOptions(options.current);
+  }, [children]);
+  useEffect(() => {
+    if (isOpen) {
+      adjustPosition();
+      searchRef.current?.focus({ preventScroll: true });
+    } else {
+      setSearchedOptions(options.current);
     }
-    const removeOption = (v: string) => {
-        onChange?.(values?.filter(x => x !== v) ?? []);
-    }
-    const selectedOptions = ((typeof values !== 'undefined' ? values : defaultValues) ?? []).map(v => options.current.find(x => x.value === v)).filter(x => !!x) as SelectOptionItemProps[];// searchedOptions.filter(x => typeof values !== 'undefined' ? values.includes(x.value.toString()) : defaultValues?.includes(x.value));
-    useOnClickOutside([ref, popupRef], handleClose);
-    useEffect(() => {
-        popupTarget.current = popupTargetId ? document.getElementById(popupTargetId) : document.body;
-    }, [popupTargetId])
-    useEffect(() => { setSearchedOptions(options.current) }, [children])
-    useEffect(() => {
-        if (isOpen) {
-            adjustPosition();
-            searchRef.current?.focus({ preventScroll: true });
-        }
-        else {
-            setSearchedOptions(options.current);
-        }
-    }, [isOpen])
-    return (
-        <div ref={wrapperRef} className={`multiselect-control ${className ? ` ${className}` : ""}${isOpen ? " is-open" : ""}${disabled ? " disabled" : ""}`}>
-            <div className="input-control">
-                {label ? <label htmlFor={name}>{label}</label> : null}
-                <div ref={ref} className={`input-wrapper`} onClick={disabled ? undefined : () => toggle(true)}>
-                    {placeholder}
-                    {isOpen ? <ChevronUp /> : <ChevronDown />}
-                    {isOpen && createPortal(<ul ref={popupRef} className="multiselect-options" style={popupStyle}>
-                        {searchable ? <li className="search-wrapper">
-                            <input ref={searchRef} type="text" onChange={onSearch} placeholder={searchText} />
-                        </li> : null}
-                        {searchedOptions?.map((x, idx) => (
-                            <li key={x.value} className={`${values?.includes(x.value) ? "selected" : ""}`} onClick={() => onSelect(x.value)}>
-                                <Opt
-                                    key={idx}
-                                    text={x.text}
-                                    value={x.value}
-                                >{x.children}</Opt>
-                            </li>
-                        ))}
-                    </ul>, popupTarget.current ?? document.body)}
-                </div>
-                {suffix}
-            </div>
-            <div className="tags">
-                {selectedOptions.map((opt, idx) => <Tag key={opt.value !== null ? opt.value : idx} closable onClose={() => removeOption(opt.value)}>
-                    {showOptionOrder && <><Badge size="small" variant="primary">{idx + 1}</Badge>{" "}</>}{opt.text ?? opt.children}
-                </Tag>)}
-            </div>
-            <input type="hidden" value={values?.join(',')} name={name} id={id} />
-        </div >
-    );
+  }, [isOpen]);
+  return (
+    <div ref={wrapperRef} className={`multiselect-control ${className ? ` ${className}` : ""}${isOpen ? " is-open" : ""}${disabled ? " disabled" : ""}`}>
+      <div className="input-control">
+        {label ? <label htmlFor={name}>{label}</label> : null}
+        <div ref={ref} className={`input-wrapper`} onClick={disabled ? undefined : () => toggle(true)}>
+          {placeholder}
+          {isOpen ? <ChevronUp /> : <ChevronDown />}
+          {isOpen &&
+            createPortal(
+              <ul ref={popupRef} className="multiselect-options" style={popupStyle}>
+                {searchable ? (
+                  <li className="search-wrapper">
+                    <input ref={searchRef} type="text" onChange={onSearch} placeholder={searchText} />
+                  </li>
+                ) : null}
+                {searchedOptions.length > 0 ? (
+                  searchedOptions?.map((x, idx) => (
+                    <li key={x.value} className={`${values?.includes(x.value) ? "selected" : ""}`} onClick={() => onSelect(x.value)}>
+                      <Opt key={idx} text={x.text} value={x.value}>
+                        {x.children}
+                      </Opt>
+                    </li>
+                  ))
+                ) : emptyComponent ? (
+                  emptyComponent
+                ) : (
+                  <Empty label={emptyLabel} />
+                )}
+              </ul>,
+              popupTarget.current ?? document.body
+            )}
+        </div>
+        {suffix}
+      </div>
+      <div className="tags">
+        {selectedOptions.map((opt, idx) => (
+          <Tag key={opt.value !== null ? opt.value : idx} closable onClose={() => removeOption(opt.value)}>
+            {showOptionOrder && (
+              <>
+                <Badge size="small" variant="primary">
+                  {idx + 1}
+                </Badge>{" "}
+              </>
+            )}
+            {opt.text ?? opt.children}
+          </Tag>
+        ))}
+      </div>
+      <input type="hidden" value={values?.join(",")} name={name} id={id} />
+    </div>
+  );
 };
 
 MultiSelect.Option = Opt;
---
+++ src/components/Shared/Empty.tsx
@@ -0,0 +1,26 @@
+import { Col, Row } from "../Griding";
+
+const Empty = ({ label="! موردی وجود ندارد" }: { label?: string }) => {
+  return (
+    <Row gutter={[0, 10]} className="empty">
+      <Col xs={24}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40">
+          <path
+            fill="#B6B6B6"
+            d="M26.984 3.333H13.017c-6.066 0-9.683 3.617-9.683 9.684v13.95c0 6.083 3.617 9.7 9.683 9.7h13.95c6.067 0 9.684-3.617 9.684-9.684V13.017c.016-6.067-3.6-9.684-9.667-9.684z"
+            opacity="0.4"
+          />
+          <path
+            fill="#B6B6B6"
+            d="M35.5 20.383h-5.8a4.23 4.23 0 00-3.816 2.367l-1.4 2.767c-.333.666-1 1.083-1.733 1.083h-5.467c-.517 0-1.25-.117-1.733-1.083l-1.4-2.75a4.278 4.278 0 00-3.817-2.367H4.501a1.16 1.16 0 00-1.167 1.167V27c0 6.05 3.633 9.667 9.7 9.667h13.967c5.716 0 9.233-3.134 9.666-8.7V21.55a1.17 1.17 0 00-1.166-1.167z"
+          />
+        </svg>
+      </Col>
+      <Col xs={24} className="text_wrap text-secondary">
+        {label}
+      </Col>
+    </Row>
+  );
+};
+
+export default Empty;
