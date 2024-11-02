6edc03769035387e77dd35fb9f8447a5f0a9d9c1
Shahrooz Bazrafshan <kingofday.b@gmail.com>
2024-11-02 16:40:19 +0330

    style: tab buttons heigth

--- src/components/Tab/Model.ts
+++ src/components/Tab/Model.ts
@@ -4,37 +4,38 @@ export interface ITabProps {
   title: ReactNode;
   children?: ReactNode;
   icon?: ReactNode;
-  disabled?:boolean;
-  className?:string
+  disabled?: boolean;
+  className?: string;
 }
 export interface ITabItem {
-    key: string;
-    icon?: ReactNode;
-    className?: string;
-    text?: ReactNode;
-    disabled?: boolean;
-    [key: string]: any;
-  }
-  export interface ITabButtonsProps {
-    id?: string;
-    variant?: "pill" | "bordered" | "ink-bar";
-    initialActiveKey?: string;
-    activeKey?: string;
-    className?: string;
-    tabs: ITabItem[];
-    hideScrollBar?: boolean;
-    onChange?: (key: string, tab?: ITabItem) => void;
-    afterChange?: (key: string) => void;
-    thresholdCentralizeTab?: number;
-    [key:string]:any
-  }
-  export interface ITabButton {
-    icon?: ReactNode;
-    className?: string;
-    onClick?: () => void;
-    activeTabKey: string;
-    tabKey: string;
-    disabled?: boolean;
-    children: ReactNode;
-    [key: string]: any;
-  }
+  key: string;
+  icon?: ReactNode;
+  className?: string;
+  text?: ReactNode;
+  disabled?: boolean;
+  [key: string]: any;
+}
+export interface ITabButtonsProps {
+  id?: string;
+  variant?: "pill" | "bordered" | "ink-bar";
+  initialActiveKey?: string;
+  activeKey?: string;
+  className?: string;
+  tabs: ITabItem[];
+  hideScrollBar?: boolean;
+  onChange?: (key: string, tab?: ITabItem) => void;
+  afterChange?: (key: string) => void;
+  thresholdCentralizeTab?: number;
+  size?: "small" | "medium" | "large";
+  [key: string]: any;
+}
+export interface ITabButton {
+  icon?: ReactNode;
+  className?: string;
+  onClick?: () => void;
+  activeTabKey: string;
+  tabKey: string;
+  disabled?: boolean;
+  children: ReactNode;
+  [key: string]: any;
+}
