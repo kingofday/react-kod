6814e9afbfa2fd1978ff60299dc8fd37d5976895
Shahrooz Bazrafshan <kingofday.b@gmail.com>
2024-12-28 12:30:30 +0330

    fix: export full screen

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -37,3 +37,4 @@ export * from './Tooltip';
 export { default as Tooltip } from './Tooltip';
 export * from './Divider';
 export { default as Divider } from './Divider';
+export { default as FullScreen } from './FullScreen';
